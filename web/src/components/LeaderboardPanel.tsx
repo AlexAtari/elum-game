34cf55a0a0dfdfc0667d98eaea67b93a0a98d28d
AlexAtari <alex100270@gmail.com>
2026-07-25 15:26:00 +0200

    tune: extend leaderboard result display

--- web/src/components/LeaderboardPanel.tsx
+++ web/src/components/LeaderboardPanel.tsx
@@ -4,7 +4,7 @@ import { useI18n } from '../i18n/I18nContext'
 import './LeaderboardPanel.css'
 
 const leaderboardRevealStepSeconds = 1
-const completeLeaderboardSeconds = 3
+const completeLeaderboardSeconds = 5
 
 type LeaderboardPanelProps = {
   roundPlayed: number
